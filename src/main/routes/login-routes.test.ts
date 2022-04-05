7e453b62cab97bf89d1644a608650c090f1b1b68
Allysson <allyssonmgs@gmail.com>
2022-04-04 21:44:53 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -4,7 +4,7 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 
 import app from '../config/app';
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -18,15 +18,17 @@ describe('Signup Routes', () => {
     await MongoHelper.disconnect();
   });
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Allysson',
-        email: 'allysson.alas@gmail.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Allysson',
+          email: 'allysson.alas@gmail.com',
+          password: '123',
+          passwordConfirmation: '123',
+        })
+        .expect(200);
+    });
   });
 });
