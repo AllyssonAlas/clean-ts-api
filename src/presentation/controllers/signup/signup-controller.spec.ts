07cee04e8de88b1433c6a70879e01563050b1baa
Allysson <allyssonmgs@gmail.com>
2022-04-08 17:13:12 -0300

    test: ensure SignUpController returns 403 if Authentication returns null

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -10,8 +10,8 @@ import {
 
 import { SignUpController } from './signup-controller';
 
-import { MissingParamError, ServerError } from '../../errors';
-import { badRequest, ok, serverError } from '../../helpers/http/http-helper';
+import { EmailInUseError, MissingParamError, ServerError } from '../../errors';
+import { badRequest, forbidden, ok, serverError } from '../../helpers/http/http-helper';
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -99,6 +99,13 @@ describe('SignUp Controller', () => {
       email: 'any_email@mail.com',
       password: 'any_password',
     });
+  });
+
+  test('Should return 403 if AddAccount returns null', async () => {
+    const { sut, addAccountStub } = makeSut();
+    jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise((resolve) => resolve(null)));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(forbidden(new EmailInUseError()));
   });
 
   test('Should return 200 if valid data is provided', async () => {
