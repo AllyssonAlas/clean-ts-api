72a297e3b6a45c3a0688eb1522eb3492115a192e
Allysson <allyssonmgs@gmail.com>
2022-04-09 19:13:50 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -22,14 +22,14 @@ describe('Survey Routes', () => {
   });
 
   describe('POST /surveys', () => {
-    test('Should return 204 on add survey success', async () => {
+    test('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
           question: 'any_question',
           answers: [{ answer: 'Answer 1', image: 'http://image-name.com' }, { answer: 'Answer 2' }],
         })
-        .expect(204);
+        .expect(403);
     });
   });
 });
