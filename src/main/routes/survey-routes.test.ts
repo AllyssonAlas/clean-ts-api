3f9cd1e5d4d1e5f74b397d66916d35aebf723a59
Allysson <allyssonmgs@gmail.com>
2022-04-15 19:11:38 -0300

    test: ensure load surveys route return 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -36,25 +36,31 @@ describe('Survey Routes', () => {
         })
         .expect(403);
     });
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Allysson',
+        email: 'allysson.alas@gmail.com',
+        password: '123',
+        role: 'admin',
+      });
+      const id = res.ops[0]._id;
+      const accessToken = sign({ id }, env.jwtSecret);
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } });
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{ answer: 'Answer 1', image: 'http://image-name.com' }, { answer: 'Answer 2' }],
+        })
+        .expect(204);
+    });
   });
 
-  test('Should return 204 on add survey with valid accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Allysson',
-      email: 'allysson.alas@gmail.com',
-      password: '123',
-      role: 'admin',
+  describe('GET /surveys', () => {
+    test('Should return 403 on load surveys without accessToken', async () => {
+      await request(app).get('/api/surveys').expect(403);
     });
-    const id = res.ops[0]._id;
-    const accessToken = sign({ id }, env.jwtSecret);
-    await accountCollection.updateOne({ _id: id }, { $set: { accessToken } });
-    await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'any_question',
-        answers: [{ answer: 'Answer 1', image: 'http://image-name.com' }, { answer: 'Answer 2' }],
-      })
-      .expect(204);
   });
 });
