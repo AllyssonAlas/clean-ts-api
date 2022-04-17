d79b96cba03a3c5b83a699dbed768f06b5e9ff05
Allysson <allyssonmgs@gmail.com>
2022-04-17 18:30:39 -0300

    refactor: add data usecases protocol files to simplify imports

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,7 +1,6 @@
 import MockDate from 'mockdate';
 
-import { SurveyModel } from '@/domain/models/survey';
-import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository';
+import { SurveyModel, LoadSurveysRepository } from './db-load-surveys-protocols';
 
 import { DbLoadSurveys } from './db-load-surveys';
 
