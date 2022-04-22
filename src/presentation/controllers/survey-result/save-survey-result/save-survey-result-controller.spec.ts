59e13e184ac0edcc7fbfcf6f45735b7b4ba13a45
Allysson <allyssonmgs@gmail.com>
2022-04-22 15:12:25 -0300

    test: ensure SaveSurveyResultController return 403 if invalid survey is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,9 +1,12 @@
-import { SaveSurveyResultController } from './save-survey-result-controller';
+import { InvalidParamError } from '@/presentation/errors';
+import { forbidden } from '@/presentation/helpers/http/http-helper';
 import {
   HttpRequest,
   LoadSurveyById,
   SurveyModel,
 } from './save-survey-result-controller-protocols';
+
+import { SaveSurveyResultController } from './save-survey-result-controller';
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -51,5 +54,14 @@ describe('SaveSurveyResult Controller', () => {
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById');
     await sut.handle(makeFakeRequest());
     expect(loadByIdSpy).toBeCalledWith('any_survey_id');
+  });
+
+  test('Should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    jest
+      .spyOn(loadSurveyByIdStub, 'loadById')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(null)));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')));
   });
 });
