0e7b24f20863b47806ace3176b056a3bac4ee6ad
Allysson <allyssonmgs@gmail.com>
2022-04-30 21:28:01 -0300

    feat: change load survey to add didAnswer field

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -6,6 +6,7 @@ import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repos
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository';
 import { LoadSurveyByIdRepository } from '@/data/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols';
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { QueryBuilder } from '@/infra/db/mongodb/helpers/query-builder';
 
 // eslint-disable-next-line prettier/prettier
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
@@ -14,9 +15,40 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     await surveyCollection.insertOne(data);
   }
 
-  async loadAll(): Promise<SurveyModel[]> {
+  async loadAll(accountId: string): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
-    const surveys = await surveyCollection.find().toArray();
+    const query = new QueryBuilder()
+      .lookup({
+        from: 'surveyResults',
+        foreignField: 'surveyId',
+        localField: '_id',
+        as: 'result',
+      })
+      .project({
+        _id: 1,
+        question: 1,
+        answers: 1,
+        date: 1,
+        didAnswer: {
+          $gte: [
+            {
+              $size: {
+                $filter: {
+                  input: '$result',
+                  as: 'item',
+                  cond: {
+                    $eq: ['$$item.accountId', new ObjectId(accountId)],
+                  },
+                },
+              },
+            },
+            1,
+          ],
+        },
+      })
+      .build();
+
+    const surveys = await surveyCollection.aggregate(query).toArray();
     return MongoHelper.mapCollection(surveys);
   }
 
